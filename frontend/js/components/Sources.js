146b3edc11b44ce9221b6c97ff930b7d6047a1f1
i.vekov <i.vekov@v-halla.ru>
2025-11-16 02:41:04 +0300

    frontend fixes

--- frontend/js/components/Sources.js
+++ frontend/js/components/Sources.js
@@ -237,7 +237,7 @@ export class Sources {
     function base64EncodeUnicode(str) {
         return btoa(
             encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
-                return String.fromCharCode('0x' + p1);
+                return String.fromCharCode(parseInt(p1, 16));
             })
         );
     }
